cdaad7342638c43af1c3d4b3d68ad13dd2bc46e4
Luz  Javier Olayo <115687904+javier0layo@users.noreply.github.com>
2024-09-30 22:52:20 -0400

    Challange1_Book_List_Database.sql

---
+++ Challange1_Book_List_Database.sql
@@ -0,0 +1,14 @@
+﻿/** Challenge: Book list database **/
+
+/**1. Create a table to store the list of books**/
+
+
+CREATE TABLE favoriteBooks(id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+INSERT INTO favoriteBooks VALUES (1, "Database Processing", 4);
+SELECT * FROM favoriteBooks;
+
+INSERT INTO favoriteBooks VALUES(2, "Computer Network and Internets", 3);
+INSERT INTO favoriteBooks VALUES(3, "Murach's Python Programming", 5);
+
+SELECT * FROM favoriteBooks;
